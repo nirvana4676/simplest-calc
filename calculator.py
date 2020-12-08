6e5d213a306c27de313d824b252fc0f5d162ef90
nirvana4676 <75182211+nirvana4676@users.noreply.github.com>
2020-12-08 19:03:00 +0200

    update
    added  divide and multiply function

--- calculator.py
+++ calculator.py
@@ -1,20 +1,30 @@
-#calculator for IT lesson
+what = input("what to do? (+ - / *): " )
 
-what = input("what to do? (+ -): " )
-#write,what operation u want to do
 a = float (input("first number: ") )
+
 b =  float (input("second number: ") )
-#write numbers,to do operations with them
-if what == "+":
+
+
+if what == "*":
+    c = a * b
+    print (c)
+
+
+elif what == "/":
+    c = a / b
+    print (c)    
+
+
+elif what == "+":
     c = a + b
     print (c)
-#prints if plus wroted
+
+
 elif what == "-":
       c = a - b
       print (c)
-#prints if minus wroted
+
 else:
-    print("wrong symbol,only =,-")
-#prints if wrong symbol wroted
+    print("wrong symbol,only + * - /")
 
 input()
